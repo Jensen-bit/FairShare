ccd0579c6e1902d7e39b22bf3dd5f4af0a89f9e1
Cole Wallace Wayne <colecrowspear11@gmail.com>
2026-04-21 22:37:55 -0600

    Updated UI and added Favicon

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -12,6 +12,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 const hbs = exphbs.create({
   extname: '.hbs',
